8c31cf37e772e21c64022a63adb797e4087e74e9
sertrosa <sertrosa@gmail.com>
2023-01-08 14:39:06 +0100

    Project update

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'HazBandeja APP'),
@@ -34,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,27 +37,23 @@ class _MyHomePageState extends State<MyHomePage> {
   double baseTriangleA = 0;
   double baseTriangleB = 0;
 
-  
-
-
-  void _incrementCounter() {
-    
+  void _calculate() {
     setState(() {
-      formulas.abC(double.parse(sidea.text), double.parse(sideb.text), double.parse(height.text), double.parse(width.text));
+      formulas.abC(double.parse(sidea.text), double.parse(sideb.text),
+          double.parse(height.text), double.parse(width.text));
       sidec = formulas.sidec;
       angleA = formulas.angleA;
       angleB = formulas.angleB;
       wingTriangleA = formulas.wingTriangleA;
       wingTriangleB = formulas.wingTriangleB;
-
     });
   }
-
 
   TextEditingController sidea = TextEditingController();
   TextEditingController sideb = TextEditingController();
   TextEditingController height = TextEditingController();
   TextEditingController width = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,42 +83,35 @@ class _MyHomePageState extends State<MyHomePage> {
               controller: width,
               keyboardType: TextInputType.number,
             ),
-            Text(
-              'Desarrollo'
-            ),
+            Text('Desarrollo'),
             Text(
               sidec.toStringAsFixed(2),
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(
-                'Ángulo A'
-            ),
+            Text('Ángulo A'),
             Text(
               angleA.toStringAsFixed(2),
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(
-                'Ángulo B'
-            ),
+            Text('Ángulo B'),
             Text(
               angleB.toStringAsFixed(2),
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(
-                'Triángulo sobre ala A'
-            ),
+            Text('Triángulo sobre ala A'),
             Text(
               wingTriangleA.toStringAsFixed(2),
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(
-                'Triángulo sobre ala B'
-            ),
+            Text('Triángulo sobre ala B'),
             Text(
               wingTriangleB.toStringAsFixed(2),
               style: Theme.of(context).textTheme.headline4,
             ),
-            ElevatedButton(onPressed: _incrementCounter,child: Text('CALCULAR'),)
+            ElevatedButton(
+              onPressed: _calculate,
+              child: Text('CALCULAR'),
+            )
           ],
         ),
       ),
